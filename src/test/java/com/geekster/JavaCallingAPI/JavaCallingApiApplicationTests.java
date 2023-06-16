5c7a2027277ac291fc9ad37618df3c5b85d5195f
SHaurya <shauryapathak595@gmail.com>
2023-06-16 09:53:04 +0530

    Initial commit

---
+++ src/test/java/com/geekster/JavaCallingAPI/JavaCallingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekster.JavaCallingAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCallingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
